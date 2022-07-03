d3dd8c7fc1345453a8246bb80fcd69692db5ced1
eduard-ops <epustynnik@gmail.com>
2022-07-03 21:26:49 +0300

    add Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,15 @@ import PropTypes from 'prop-types';
 
 import { useState } from 'react';
 
+import { toast } from 'react-toastify';
+
 import { useGetContactsQuery } from 'redux/contacts/contactsRtkSlice';
+
+import { BootstrapInput as Input } from 'UI/stylesUi';
+
+import { CssLabel as Label } from 'UI/stylesUi';
+
+import { ButtonContacts as AddButton } from 'UI/stylesUi';
 
 export default function Form({ onSubmit }) {
   const [name, setName] = useState('');
@@ -33,7 +41,8 @@ export default function Form({ onSubmit }) {
 
   const cheakAddContact = name => {
     const isValidate = contacts.find(item => item.name === name);
-    isValidate && alert(`${name} is already in contacts`);
+    isValidate &&
+      toast.info(`${name} is already in contacts ⚠`, { theme: 'colored' });
     return isValidate;
   };
 
@@ -46,38 +55,40 @@ export default function Form({ onSubmit }) {
     resetForm();
   };
 
+  const inspectInpusts = () => {
+    if (name.trim() === '' || number.trim() === '') return true;
+  };
+
   return (
     <form onSubmit={handleSubmut} className={s.form}>
-      <label className={s.label}>
+      <Label shrink htmlFor="bootstrap-input">
         Name
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          value={name}
-          onChange={handleChange}
-        />
-      </label>
-
-      <label className={s.label}>
-        Телефон
-        <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={handleChange}
-          value={number}
-        />
-      </label>
-
-      <button className={s.button} type="submit">
-        {' '}
+      </Label>
+      <Input
+        id="bootstrap-input"
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={handleChange}
+      />
+      <Label shrink htmlFor="bootstrap-input">
+        Phone
+      </Label>
+      <Input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        onChange={handleChange}
+        value={number}
+      />
+      <AddButton disabled={inspectInpusts()} variant="contained" type="submit">
         Add contact
-      </button>
+      </AddButton>
     </form>
   );
 }
