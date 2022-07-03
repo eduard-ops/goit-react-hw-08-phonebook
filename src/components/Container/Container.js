e923a2982b54e284f2793836c18c681fa254e47f
eduard-ops <epustynnik@gmail.com>
2022-07-03 21:25:36 +0300

    update Container.js

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,11 +1,5 @@
 import s from './Container.module.css';
 
-import PropTypes from 'prop-types';
-
 export default function Container({ children }) {
   return <div className={s.container}>{children}</div>;
 }
-
-Container.propTypes = {
-  children: PropTypes.array,
-};
