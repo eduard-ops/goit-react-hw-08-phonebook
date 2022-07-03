e0101a89589ee0e862128b1792ac1ce99c73e31e
eduard-ops <epustynnik@gmail.com>
2022-07-03 21:29:43 +0300

    add HomePage\HomePage.js

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import s from './HomePage.module.css';
+
+export default function HomePage() {
+  return (
+    <div className={s.wrap}>
+      <h1 className={s.text}>Home Page</h1>;
+    </div>
+  );
+}
