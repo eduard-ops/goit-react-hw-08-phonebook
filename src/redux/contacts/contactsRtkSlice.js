68247da3722c164a35cd90290b504eaf6f7d873f
eduard-ops <epustynnik@gmail.com>
2022-07-03 21:31:15 +0300

    add contactsRtkSlice.js

--- src/redux/contacts/contactsRtkSlice.js
+++ src/redux/contacts/contactsRtkSlice.js
@@ -1,11 +1,32 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
+
+import axios from 'axios';
+
+const axiosBaseQuery =
+  ({ baseUrl } = { baseUrl: '' }) =>
+  async ({ url, method, data, params }) => {
+    try {
+      const result = await axios({ url: baseUrl + url, method, data, params });
+      console.log(result);
+      return { data: result.data };
+    } catch (axiosError) {
+      let err = axiosError;
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      };
+    }
+  };
 
 export const contactsSplice = createApi({
   // The cache reducer expects to be added at `state.api` (already default - this is optional)
   reducerPath: 'contacts',
   // All of our requests will have URLs starting with '/fakeApi'
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://62acce14402135c7acb94c1c.mockapi.io/api/contacts/',
+  baseQuery: axiosBaseQuery({
+    baseUrl: 'https://connections-api.herokuapp.com',
+    keepUnusedDataFor: 0,
   }),
   tagTypes: ['Contact'],
   // The "endpoints" represent operations and requests for this server
@@ -13,15 +34,14 @@ export const contactsSplice = createApi({
     // The `getPosts` endpoint is a "query" operation that returns data
     getContacts: builder.query({
       // The URL for the request is '/fakeApi/posts'
-      query: () => '/contacts',
+      query: () => ({ url: '/contacts', method: 'GET' }),
       providesTags: ['Contact'],
+      keepUnusedDataFor: 0,
     }),
     addContact: builder.mutation({
-      query: data => ({
-        url: `/contacts`,
-        method: 'POST',
-        body: data,
-      }),
+      query: contact => {
+        return { url: '/contacts', method: 'POST', data: contact };
+      },
       invalidatesTags: ['Contact'],
     }),
     deleteContact: builder.mutation({
