6774abaf9236937a3439fcf191432029f7dde7d0
eduard-ops <epustynnik@gmail.com>
2022-07-03 21:26:12 +0300

    add Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,17 @@
 import s from './Filter.module.css';
 
+import { BootstrapInput as FilterInput } from 'UI/stylesUi';
+
 import PropTypes from 'prop-types';
 
 export default function Filter({ value, onChange }) {
   return (
-    <label className={s.label}>
-      Find Contacts by name
-      <input value={value} onChange={e => onChange(e.target.value)} />
-    </label>
+    <div className={s.wrap}>
+      <h2>Find Contacts by name</h2>
+      <label className={s.label}>
+        <FilterInput value={value} onChange={e => onChange(e.target.value)} />
+      </label>
+    </div>
   );
 }
 
