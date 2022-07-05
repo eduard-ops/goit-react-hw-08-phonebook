d9387993d0eadeb64e169723a0e71537c17210b0
eduard-ops <epustynnik@gmail.com>
2022-07-05 22:49:22 +0300

    add PrivateRoute.js

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -5,6 +5,7 @@ import authSelectors from 'redux/auth/auth-selectors';
 export function PrivateRoute({ redirectTo }) {
   const isLoggedIn = useSelector(authSelectors.getIsLooggedIn);
   const isUser = useSelector(authSelectors.getUsername);
-  const inspect = isLoggedIn || isUser;
+  console.log(isUser);
+  const inspect = isUser;
   return inspect ? <Outlet /> : <Navigate to={redirectTo} />;
 }
