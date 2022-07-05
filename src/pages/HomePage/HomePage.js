141b272e4a00599f004500343a7207a48cc85585
eduard-ops <epustynnik@gmail.com>
2022-07-05 22:48:48 +0300

    delete home

--- src/pages/HomePage/HomePage.js
+++
@@ -1,9 +0,0 @@
-import s from './HomePage.module.css';
-
-export default function HomePage() {
-  return (
-    <div className={s.wrap}>
-      <h1 className={s.text}>Home Page</h1>;
-    </div>
-  );
-}
