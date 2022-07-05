def92831207ed09ce06fd0d9c7d39720b8729bae
eduard-ops <epustynnik@gmail.com>
2022-07-05 22:53:20 +0300

    fix

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -4,8 +4,6 @@ import authSelectors from 'redux/auth/auth-selectors';
 
 export function PrivateRoute({ redirectTo }) {
   const isLoggedIn = useSelector(authSelectors.getIsLooggedIn);
-  const isUser = useSelector(authSelectors.getUsername);
-  console.log(isUser);
-  const inspect = isUser;
-  return inspect ? <Outlet /> : <Navigate to={redirectTo} />;
+
+  return isLoggedIn ? <Outlet /> : <Navigate to={redirectTo} />;
 }
